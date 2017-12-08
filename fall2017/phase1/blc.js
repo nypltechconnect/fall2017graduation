72aa7a2073882ed20fc88a8e13315572bdabdd10
Kurt Petrek <kurtpetrek@gmail.com>
2017-12-08 13:49:29 -0500

    adds sheldon blc phase 1 class

--- fall2017/phase1/blc.js
+++ fall2017/phase1/blc.js
@@ -14,5 +14,16 @@ projects = [
       ["Brooklyn Bridge: Linnette, Elio, and Vivian", "https://epichardo85.github.io/brooklynbridge/"],
       ["Tokyo Travels: Mayra, Katrina, Travern", "https://kmontgomery88.github.io/tokyotravels/"]
     ]
+  },
+  {
+    Location: "Bronx Library Center Monday & Thursday 2 PM - 4PM",
+    Instructor: "Sheldon Norman",
+    "Personal Projects": [
+      ["Polina Nikolova", "https://polina08.github.io/GreenDreams/"],
+      ["Tara Ellicott", "https://taraellicott.github.io/alleycat"],
+      ["Eduardo Balladares Torres", "https://n0de96.github.io/Project/"],
+      ["John Tirado", "https://yugiohno.github.io/parks/"]
+    ],
+    "Group Projects": [["Planets in Urban Skies", "https://polina08.github.io/Planets/"]]
   }
 ];
