84971e55d61cc3bf5a99b2d96e810fce15cfad73
Kurt Petrek <kurtpetrek@gmail.com>
2017-12-08 13:05:56 -0500

    adds Steven SASB Phase 1

---
+++ fall2017/phase1/sasb.js
@@ -0,0 +1,21 @@
+projects = [
+  {
+    Location: "SASB Tuesday & Wednesday 1:30PM - 3:30PM",
+    Instructor: "Steven Deolus",
+    "Personal Projects": [
+      ["Purnima Lakshmi Narayanan", "https://madrasgal.github.io/ProjectCode-FinalProject/"],
+      ["Dato Mio", "https://dmuptown.github.io/Artsy/"],
+      ["Emery Thompson", "https://bellaroxanne.github.io/nyboardsite/"],
+      ["Kristof Wisniowiecki", "https://kriskorybut.github.io/KKW-Project-Code-Project/"],
+      ["Lea Cambra", "https://leacambra.github.io/lcc-project/"],
+      ["Sophie Fels", "https://coding4bginners.github.io/CodingForBeginners/"],
+      ["Rose Vrabel", "https://rdvrabel.github.io/personalwebsite1/index.html"],
+      ["Mary Yu", "https://maryliangyee.github.io/november2017/"]
+    ],
+    "Group Projects": [
+      ["Lets Code", "https://dmuptown.github.io/codecollab/"],
+      ["The Beach Front Hotel", "https://madrasgal.github.io/ProjectCode-CollabProject/"],
+      ["Storm City", "https://coding4bginners.github.io/GroupProject/"]
+    ]
+  }
+];
